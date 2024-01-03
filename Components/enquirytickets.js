7328fd8c9f46db50e8a27fb7cbbbaa49846ab762
tusharxp <tushar.tomar@travelxp.tv>
2024-01-03 19:28:56 +0530

    screen 1 flight img left

---
+++ Components/enquirytickets.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Flightandhotelticket from "./flightandhotelticket";
+
+export default function enquirytickets() {
+  return (
+    <div className=''>
+        <h1 className='pl-2 mt-4 mb-6 text-lg font-semibold'>Enquiry Tickets</h1>
+      <Flightandhotelticket />
+      <Flightandhotelticket />
+      <Flightandhotelticket />
+    </div>
+  )
+}
